56afa89ff4948c259893c0ab85fb29b581aca5dd
Lewis Lovette <lovettel@coventry.ac.uk>
2019-11-05 12:09:27 +0000

    firebase signup works, sends new user details (email, pass) to firebase.

--- app/src/main/java/com/example/lewislovette_7814291/SignupPage.java
+++ app/src/main/java/com/example/lewislovette_7814291/SignupPage.java
@@ -1,13 +1,172 @@
 package com.example.lewislovette_7814291;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignupPage extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    TextView statusText;
+    TextView detailText;
+    EditText email;
+    EditText password;
+    Button signup;
+    String TAG = "Firebase Auth";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+
+        statusText = findViewById(R.id.statusText);
+        detailText = findViewById(R.id.detailsText);
+
+        email = findViewById(R.id.emailInput);
+        password = findViewById(R.id.passwordInput);
+        signup = findViewById(R.id.signup);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                createAccount(email.getText().toString(), password.getText().toString());
+            }
+        });
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+
+    private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        //showProgressDialog();
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getBaseContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        //hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        //showProgressDialog();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getBaseContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            statusText.setText("AUTH FAILED");
+                        }
+                        //hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+
+
+
+    private void updateUI(FirebaseUser user) {
+
+        if (user != null) {
+            Log.v(TAG, "Signed in Successfully");
+        } else {
+            Log.v(TAG, "Failed to Signed in");
+            //Open activity
+        }
+    }
+
+    private boolean validateForm() {
+        boolean valid = true;
+
+        String email = this.email.getText().toString();
+        if (TextUtils.isEmpty(email)) {
+            this.email.setError("Required.");
+            valid = false;
+        } else {
+            this.email.setError(null);
+        }
+
+        String password = this.password.getText().toString();
+        if (TextUtils.isEmpty(password)) {
+            this.password.setError("Required.");
+            valid = false;
+        } else {
+            this.password.setError(null);
+        }
+
+        return valid;
+    }
+
 }
