a54e1f89ce3d4ac1ba517113ef0a4dda6993c9bf
Lewis Lovette <lewislovette3@hotmail.co.uk>
2019-12-10 17:19:15 +0000

    tests added.

---
+++ app/src/test/java/com/example/lewislovette_7814291/NoteModelTest.java
@@ -0,0 +1,29 @@
+package com.example.lewislovette_7814291;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NoteModelTest {
+
+  @Test
+  public void getInstance() {}
+
+  @Test
+  public void setView() {}
+
+  @Test
+  public void getEmail() {}
+
+  @Test
+  public void setEmail() {}
+
+  @Test
+  public void getNotes() {}
+
+  @Test
+  public void addNote() {}
+
+  @Test
+  public void deleteNote() {}
+}
---
+++ app/src/test/java/com/example/lewislovette_7814291/UsersModelTest.java
@@ -0,0 +1,90 @@
+package com.example.lewislovette_7814291;
+
+import static junit.framework.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+
+import android.content.Context;
+import android.provider.ContactsContract.Data;
+import android.view.View;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+
+public class UsersModelTest {
+
+  View view = Mockito.mock(View.class);
+  UsersModel usersModel = UsersModel.getInstance();
+
+  @Rule
+  public MockitoRule rule = MockitoJUnit.rule();
+
+
+  @Test
+  public void getInstance() {
+    UsersModel usersModel = UsersModel.getInstance();
+
+    assertEquals(usersModel.getEmail(), null);
+  }
+
+  @Test
+  public void setView() {}
+
+  @Test
+  public void hasPicture() {}
+
+  @Test
+  public void getProfilePic() {}
+
+  @Test
+  public void setProfilePic() {}
+
+  @Test
+  public void addUser() {
+  }
+
+  @Test
+  public void exists() {}
+
+  @Test
+  public void getEmail() {
+    UsersModel usersModel = UsersModel.getInstance();
+    usersModel.setEmail("testemail@email.com");
+
+    assertEquals(usersModel.getEmail(), "testemail@email.com");
+  }
+
+  @Test
+  public void setEmail() {
+    UsersModel usersModel = UsersModel.getInstance();
+    usersModel.setEmail("testemail@email.com");
+
+    assertEquals(usersModel.getEmail(), "testemail@email.com");
+  }
+
+  @Test
+  public void getPassword() {
+    UsersModel usersModel = UsersModel.getInstance();
+    usersModel.setPassword("password");
+
+    assertEquals(usersModel.getPassword(), "password");
+  }
+
+  @Test
+  public void setPassword() {
+    UsersModel usersModel = UsersModel.getInstance();
+    usersModel.setPassword("password");
+
+    assertEquals(usersModel.getPassword(), "password");
+  }
+
+}
