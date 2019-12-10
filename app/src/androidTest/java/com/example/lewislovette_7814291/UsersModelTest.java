eed632002409317d28d8a09b3983c54aad17bfb5
Lewis Lovette <lewislovette3@hotmail.co.uk>
2019-12-10 13:32:14 +0000

    google style setup, usermodel tests setup

---
+++ app/src/androidTest/java/com/example/lewislovette_7814291/UsersModelTest.java
@@ -0,0 +1,41 @@
+package com.example.lewislovette_7814291;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UsersModelTest {
+
+  @Test
+  public void getInstance() {}
+
+  @Test
+  public void setView() {}
+
+  @Test
+  public void hasPicture() {}
+
+  @Test
+  public void getProfilePic() {}
+
+  @Test
+  public void setProfilePic() {}
+
+  @Test
+  public void addUser() {}
+
+  @Test
+  public void exists() {}
+
+  @Test
+  public void getEmail() {}
+
+  @Test
+  public void setEmail() {}
+
+  @Test
+  public void getPassword() {}
+
+  @Test
+  public void setPassword() {}
+}
